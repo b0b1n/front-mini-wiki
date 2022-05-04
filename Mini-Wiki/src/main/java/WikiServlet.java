c4c6f5b61f6a5e4e00518207698f2ed8d22aa274
b0b1n <sohaibskious@gmail.com>
2022-05-04 21:51:52 +0200

    Passage des posts vers la page d accueil avec succès

--- Mini-Wiki/src/main/java/WikiServlet.java
+++ Mini-Wiki/src/main/java/WikiServlet.java
@@ -1,11 +1,18 @@
 
-
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
 
 /**
  * Servlet implementation class WikiServlet
@@ -14,25 +21,48 @@ import java.io.IOException;
 public class WikiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public WikiServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Default constructor.
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.sendRedirect("view/single.jsp");
+	public WikiServlet() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		try {
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest hr = HttpRequest.newBuilder().uri(URI.create("http://127.0.0.1:8000/accueil"))
+					.header("Content-type", "application/json").build();
+			HttpResponse<String> hrep;
+			hrep = client.send(hr, BodyHandlers.ofString());
+			String mesPosts = hrep.body();
+
+			//System.out.println(mesPosts);
+
+			JsonArrayBuilder job = Json.createArrayBuilder();
+			job.add(mesPosts);
+			request.setAttribute("posts", job.build());
+
+			this.getServletContext().getRequestDispatcher("/view/index.jsp").forward(request, response);
+
+		} catch (IOException | InterruptedException e) {
+			System.out.println("Erreur !! Lors de récuperation des données");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
